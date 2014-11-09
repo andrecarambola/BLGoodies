04ee929269074b2241b6d18056cd22a7dc165e98
André Abou Chami Campana <campana.andre@gmail.com>
2014-11-09 01:30:57 -0200

    Implemented Goals query. Added images.

---
+++ Goodies/Date/NSDate+BLDate.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+BLDate.h
+//  Project
+//
+//  Created by André Abou Chami Campana on 09/11/14.
+//  Copyright (c) 2014 Bell App Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSDate (BLDate)
+
++ (NSDate *)firstDayOfNextMonth;
+
+@end
---
+++ Goodies/Date/NSDate+BLDate.m
@@ -0,0 +1,21 @@
+//
+//  NSDate+BLDate.m
+//  Project
+//
+//  Created by André Abou Chami Campana on 09/11/14.
+//  Copyright (c) 2014 Bell App Lab. All rights reserved.
+//
+
+#import "NSDate+BLDate.h"
+
+
+@implementation NSDate (BLDate)
+
++ (NSDate *)firstDayOfNextMonth
+{
+    NSDate *result = [NSDate date];
+    
+    NSDateComponents *dateComponents = [NSLocale currentLocale] cal
+}
+
+@end
